1547204f53ea6d472d25c2d6034993ab312fa522
prashant9917 <prashantsagar452@gmail.com>
2023-11-23 14:44:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function btnFunction() {
+    document.getElementById('knowMore').innerHTML = `At the moment we have ball-by-ball information for X 15,
+    645 matches comprising 799 Test matches, 23 other multi-day matches,
+    2,816 One-day internationals, 426 other one-day matches, 3,037 T20 internationals,
+    329 international T20s, 23 Afghanistan Premier League matches, 535 Big Bash League matches,
+    17 T20 Blaze matches, 344 Bangladesh Premier League matches, 47 Bob Willis Trophy matches, 
+    1,107 County Championship matches, 82 Charlotte Edwards Cup matches, 
+    341 Caribbean Premier League matches, 284 CSA T20 Challenge matches, 
+    39 FairBreak Invitational Tournament matches, 187 The Hundred matches, 
+    32 International League T20 matches, 1,024 Indian Premier League matches, 
+    22 Cricket Ireland Inter-Provincial Limited Over Cup matches, 
+    40 Cricket Ireland Inter-Provincial Twenty20 Trophy matches,
+    95 Lanka Premier League matches, 19 Major League Cricket matches,
+    56 Mzansi Super League matches, 1,197 T20 Blast matches, 248 Pakistan Super League matches, 
+    131 Rachael Heyhoe Flint Trophy matches, 659 One-Day Cup matches, 33 SA20 matches, 8 Super 
+    50 matches, 687 Syed Mushtaq Ali Trophy matches, 125 Sheffield Shield matches, 288 Super Smash
+    matches, 404 Women's Big Bash League matches, 11 Women's Caribbean Premier League matches, 
+    22 Women's Premier League matches, 95 Women's Cricket Super League matches, and 13 Women's T20
+    Challenge matches featuring 104 countries, 235 club teams, and 3 representative XIs going back as far as 
+    2003 (for women), and 2002 (for men).`
+    ;
+}
